1e02e3ba97b0fd13e404126ec3a5f30f0d9a9aaa
Denis Borisov <mai.borisov.denis@gmail.com>
2024-09-23 01:35:38 +0300

    Remove files that should be ignored

--- lab1/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/maibo/CLionProjects/cpp-workshop/lab1
-# Build directory: C:/Users/maibo/CLionProjects/cpp-workshop/lab1/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("Google_tests")
