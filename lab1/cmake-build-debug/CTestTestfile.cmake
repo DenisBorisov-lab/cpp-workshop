4c439c88e27afd2d75be59c265b4a8a48f8cae4d
Denis Borisov <mai.borisov.denis@gmail.com>
2024-09-23 01:27:18 +0300

    init

---
+++ lab1/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/maibo/CLionProjects/cpp-workshop/lab1
+# Build directory: C:/Users/maibo/CLionProjects/cpp-workshop/lab1/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("Google_tests")
