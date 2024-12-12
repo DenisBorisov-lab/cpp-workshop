7ffd8301274bc39c4af2d070569f6dfcb0898bc9
Denis Borisov <mai.borisov.denis@gmail.com>
2024-12-12 18:02:44 +0300

    небольшие доработки

---
+++ lab-2/cmake-build-debug/Google_tests/lib/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/maibo/CLionProjects/cpp-workshop/lab-2/Google_tests/lib/googlemock
+# Build directory: C:/Users/maibo/CLionProjects/cpp-workshop/lab-2/cmake-build-debug/Google_tests/lib/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
